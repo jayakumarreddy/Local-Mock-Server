11cc4123290910861f455bc3d2631eb54c68c3bf
jayakumarreddy <rjayakumarreddy887@gmail.com>
2020-05-24 09:05:45 +0530

    Testing the Electron auto updater

--- src/pages/ReqForm/index.js
+++ src/pages/ReqForm/index.js
@@ -63,11 +63,7 @@ const ReqForm = ({
       <div className="sheet-inner">
         <div className="sheet-req-wrapper">
           <Label fontSize={"24px"} color={"white"}>
-            Request :
-            <span className="sheet-inner-info">
-              (To acces mock server hit port 8000 Ex:
-              http://localhost:8000/users)
-            </span>
+            Request :<span className="sheet-inner-info"></span>
           </Label>
           <div className="sheet-request">
             <div>
